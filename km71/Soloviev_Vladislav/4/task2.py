d2c7ab909bf96b8957e5be8841f2a7704b39457a
Slavicscheise <vladik9191@gmail.com>
2017-11-02 20:05:40 +0200

    4
    Тема: Конструкції керування.

---
+++ km71/Soloviev_Vladislav/4/task2.py
@@ -0,0 +1,9 @@
+x = int(input("Enter x:"))
+
+if x > 0:
+    answer = 1
+elif x<0:
+    answer = -1
+else:
+    answer = 0
+print("sign(x) = ", answer) 
---
+++ km71/Soloviev_Vladislav/4/task3.py
@@ -0,0 +1,6 @@
+# Task3
+a = int(input("Enter the first number\n"))
+b = int(input("Enter the second number\n"))
+c = int(input("Enter the third number\n"))
+
+print(min(min(a,b),c))
